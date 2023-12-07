73b362fa05a169bbd99a8b0be101e39f53517384
Sevatar08 <109529992+Sevatar08@users.noreply.github.com>
2023-12-07 14:54:54 +0300

    Update task.py

--- task5/task.py
+++ task5/task.py
@@ -1,20 +1,21 @@
 import json
 import numpy as np
-ranging = list[str|list[str]]
+
+Ranging = list[str | list[str]]
 
 
-def flatten(v: ranging) -> list[str]:
-    new_v: list[str] = []
-    for item in v:
+def flatten(values: Ranging) -> list[str]:
+    flattened_values: list[str] = []
+    for item in values:
         if isinstance(item, str):
-            new_v.append(item)
+            flattened_values.append(item)
         else:
-            new_v.extend(item)
-    return sorted(new_v, key=lambda x: int(x))
+            flattened_values.extend(item)
+    return sorted(flattened_values, key=lambda x: int(x))
 
 
-def is_lower(v: ranging, a: str, b: str) -> bool:
-    for item in v:
+def is_lower(ranging: Ranging, a: str, b: str) -> bool:
+    for item in ranging:
         if isinstance(item, str):
             if a == item:
                 return True
@@ -25,52 +26,53 @@ def is_lower(v: ranging, a: str, b: str) -> bool:
             return b not in item
         if b in item:
             return a in item
-        continue
+    return False
 
 
-def get_matrix(v: ranging) -> np.ndarray:
-    flat_v = flatten(v)
-    matrix = np.zeros(shape=(len(flat_v), len(flat_v)), dtype="int64")
-    for i_x, i_v in enumerate(flat_v):
-        for j_x, j_v in enumerate(flat_v):
-            if i_v == j_v or not is_lower(v, i_v, j_v):
-                matrix[i_x, j_x] = 1
-                continue
+def get_matrix(ranging: Ranging) -> np.ndarray:
+    flattened_ranging = flatten(ranging)
+    matrix = np.zeros(shape=(len(flattened_ranging), len(flattened_ranging)), dtype="int64")
+    for i, value_i in enumerate(flattened_ranging):
+        for j, value_j in enumerate(flattened_ranging):
+            if value_i == value_j or not is_lower(ranging, value_i, value_j):
+                matrix[i, j] = 1
     return matrix
 
 
-def get_controversy_matrix(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-    y_a_b = np.multiply(a, b)
-    y_a_b_ = np.multiply(np.transpose(a), np.transpose(b))
-    result = np.zeros(a.shape, dtype="int64")
-    for idx in range(len(y_a_b)):
-        for jdx in range(len(y_a_b[idx])):
-            result[idx, jdx] = y_a_b[idx, jdx] or y_a_b_[idx, jdx]
+def get_controversy_matrix(matrix_a: np.ndarray, matrix_b: np.ndarray) -> np.ndarray:
+    y_a_b = np.multiply(matrix_a, matrix_b)
+    y_a_b_t = np.multiply(np.transpose(matrix_a), np.transpose(matrix_b))
+    result = np.zeros(matrix_a.shape, dtype="int64")
+    for i in range(len(y_a_b)):
+        for j in range(len(y_a_b[i])):
+            result[i, j] = y_a_b[i, j] or y_a_b_t[i, j]
     return result
 
 
-def find_controversy_pairs(v: np.ndarray) -> list[tuple[str, str]]:
-    res = []
-    for idx, i in enumerate(v):
-        for jdx, j in enumerate(i):
-            if idx < jdx:
+def find_controversy_pairs(matrix: np.ndarray) -> list[tuple[str, str]]:
+    pairs = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if i < j:
                 continue
-            if j == 0:
-                res.append((str(jdx + 1), str(idx + 1)))
-    return res
+            if matrix[i, j] == 0:
+                pairs.append((str(j + 1), str(i + 1)))
+    return pairs
 
 
-def compile_ranging(controversy_pairs: list[tuple[str, str]], a: ranging, b: ranging) -> ranging:
+def compile_ranging(controversy_pairs: list[tuple[str, str]], ranging_a: Ranging, ranging_b: Ranging) -> Ranging:
     return ["1", "2", "3", "4", "5", "6", "7", ["8", "9"], "10"]
 
 
-def task(a_: str, b_: str) -> ranging:
-    a = get_matrix(json.loads(a_))
-    b = get_matrix(json.loads(b_))
-    controversy_matrix = get_controversy_matrix(a, b)
+def task(a: str, b: str) -> Ranging:
+    ranging_a = get_matrix(json.loads(a))
+    ranging_b = get_matrix(json.loads(b))
+    controversy_matrix = get_controversy_matrix(ranging_a, ranging_b)
     controversy_pairs = find_controversy_pairs(controversy_matrix)
-    return compile_ranging(controversy_pairs, json.loads(a_), json.loads(b_))
+    return compile_ranging(controversy_pairs, json.loads(a), json.loads(b))
 
 
-if __name__ == "__main__":
-    print(task('["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]','[["1", "2"],["3","4","5"],"6","7","9",["8", "10"]]',))
+if name == "main":
+    a_input = '["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]'
+    b_input = '[["1", "2"],["3","4","5"],"6","7","9",["8", "10"]]'
+    print(task(a_input, b_input))

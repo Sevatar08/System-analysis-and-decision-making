b25ee3f6b09baff608b82d944d4dd94dcc75ff46
Sevatar08 <109529992+Sevatar08@users.noreply.github.com>
2023-12-17 19:32:00 +0300

    Update task.py

--- task2/task.py
+++ task2/task.py
@@ -1,44 +1,66 @@
-from io import StringIO
 import csv
+import argparse
 
-def bfs(graph, result, cur, prev, neighbours):
-    parent = 1 if (prev > 0) else 0
-    result[cur][1] += parent
-    result[cur][3] += prev - parent
-    result[cur][0] = len(graph[cur])
-    result[cur][4] = neighbours
-    for child in graph[cur]:
-        result[cur][2] += bfs(graph, result, child, prev + 1, len(graph[cur]) - 1)
-    result[cur][2] -= result[cur][0]
-    return result[cur][2] + result[cur][0] + 1
+def read(filepath):
+    dict = {}
+    with open(filepath, 'r') as table:
+        reader = csv.reader(table)
+        for row in reader:
+            if row[0] not in dict:
+                dict[row[0]] = row[1:]
+            else:
+                dict[row[0]].extend(row[1])
+    return dict
 
-def task(csvString):
-    f = StringIO(csvString)
-    reader = csv.reader(f, delimiter=',')
-    graph = dict()
-    nodes = set()
-    starts = set()
-    not_starts = set()
-    for row in reader:
-      a = row[0]
-      b = row[1]
-      if a in graph:
-          graph[a].append(b)
-      else:
-          graph[a] = [b]
-      nodes.add(a)
-      nodes.add(b)    
-      not_starts.add(b)
-    for node in nodes:
-        if node not in graph:
-            graph[node] = []
-        if node not in not_starts:
-            starts.add(node)
-    result = { node: [0, 0, 0, 0, 0] for node in nodes }
-    for start in starts:
-        bfs(graph, result, start, 0, 0)
-    keys = sorted(result.keys())
-    values = [result[key] for key in keys]
-    return '\n'.join([",".join(str(el) for el in value) for value in values])
+def keys(graph):
+    all_keys = []
+    for key in graph:
+        if key not in all_keys:
+            all_keys.append(key)
+        for elem in graph[key]:
+            if elem not in all_keys:
+                all_keys.append(elem)
+    return all_keys, len(all_keys)
 
-print(task("1,2\n2,3\n2,4\n3,5\n3,6"))
+def matrix(graph, ln):
+    matrix = [[0 for _ in range(ln)] for _ in range(ln)]
+    for k in graph:
+        for v in graph[k]:
+            matrix[int(k) - 1][int(v) - 1] = 1
+            matrix[int(v) - 1][int(k) - 1] = -1
+    return matrix
+
+def csv(matrix, file_name='task_res.csv'):
+    with open(file_name, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        for row in matrix:
+            writer.writerow(row)
+        
+def task(filename):
+    graph = read(filename)
+    _, ln = keys(graph)
+    matrix = matrix(graph, ln)
+    result = [[0 for _ in range(5)] for _ in range(ln)]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                result[i][0] += 1
+                for index, value in enumerate(matrix[j]):
+                    if value == 1:
+                        result[i][2] += 1
+            if matrix[i][j] == -1:
+                result[i][1] += 1
+                for index, value in enumerate(matrix[j]):
+                    if value == -1:
+                        result[i][3] += 1
+                    if value == 1 and index != i:
+                        result[i][4] += 1
+    csv(result)
+    
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+    return parser.parse_args()
+
+args = parser()
+task(args.filepath)

bd13f11466455cd7492c5933cc21055c8de1e1ab
Sevatar08 <109529992+Sevatar08@users.noreply.github.com>
2023-12-18 21:03:31 +0300

    Update task.py

--- task4/task.py
+++ task4/task.py
@@ -1,55 +1,84 @@
-import numpy as np
+from math import log2
 
-def entropiya(t):
-    if (t != 0):
-      return -t * np.log2(t)
-    else:
-      return 0
+def get_vals():
+    values_sum = []
+    values_prod = []
+    for i in range(1, 7):
+        for j in range(1, 7):
+            sum_ij = i + j
+            prod_ij = i * j
+            values_sum.append(sum_ij)
+            values_prod.append(prod_ij)
+    values_sum = list(set(values_sum))
+    values_prod = list(set(values_prod))
+    matrix = [[0 for _ in range(len(values_prod))] for _ in range(len(values_sum))]
+    for i in range(1, 7):
+        for j in range(1, 7):
+            sum_ij = i + j
+            prod_ij = i * j
+            matrix[values_sum.index(sum_ij)][values_prod.index(prod_ij)] += 1
+    return matrix
 
-def toFixed(t):
-    return format(t, '.2f')
+def prmatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            matrix[i][j] /= 36
+    return matrix
 
-def task4():
-  min_val = 1
-  max_val = 6
-  all_var_count = max_val * max_val
-  mults_count = all_var_count
-  sums_count = max_val * 2 - min_val * 2 + 1
-  var = [[0 for j in range(mults_count)] for i in range(sums_count)]
-  mult_norm = 1
-  sum_norm = 2
-  used_cols = set()
-  
-  for i in range(min_val, max_val + 1):
-    for j in range(min_val, max_val + 1):
-      cur_mult = i * j
-      cur_sum = i + j
-      used_cols.add(cur_mult - mult_norm)
-      var[cur_sum - sum_norm][cur_mult - mult_norm] += 1
-      
-  resized_var = []
-  
-  for i in range(sums_count):
-    cur_row = []
-    for j in range(mults_count):
-      if (j in used_cols):
-        cur_row.append(var[i][j])
-    resized_var.append(cur_row)
-    
-  matrix = np.array(resized_var)
-  mults_count = len(resized_var[0])
-  matrix = matrix * 1.0 / all_var_count
-  PA = matrix.sum(axis=1)
-  PB = matrix.sum(axis=0) 
-  vect_ent = np.vectorize(entropiya)
-  matrix = vect_ent(matrix)
-  PA = vect_ent(PA)
-  PB = vect_ent(PB)
-  HA = np.sum(PA)
-  HB = np.sum(PB)
-  HAB = np.sum(matrix)
-  HaB = HAB - HA
-  HI = HB - HaB
-  return [toFixed(HAB), toFixed(HA), toFixed(HB), toFixed(HaB), toFixed(HI)]
+def sentropy(matrix, by='row'):
+    entropy = 0.0
+    if by == 'row':
+        for i in range(len(matrix)):
+            h = 0.0
+            for value in matrix[i]:
+                if value != 0.0:
+                    h += value
+            h = -1 * h * log2(h)
+            entropy += h
+    elif by == 'column':
+        for i in range(len(matrix[0])):
+            h = 0.0
+            for j in range(len(matrix)):
+                if matrix[j][i] != 0.0:
+                    h += matrix[j][i]
+            h = -1 * h * log2(h)
+            entropy += h
+    return entropy
 
-print(task4())
+def jentropy(matrix):
+    entropy = 0.0
+    for i in range(len(matrix)):
+        h = 0.0
+        for j in range(len(matrix[i])):
+            if matrix[i][j] != 0.0:
+                h += -1 * matrix[i][j] * log2(matrix[i][j])
+        entropy += h
+    return entropy
+
+def centropy(matrix, prob_matrix):
+    prob_matrix_cond = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            prob_matrix_cond[i][j] = matrix[i][j] / sum(matrix[i])
+    entropy = 0.0
+    for i in range(len(prob_matrix_cond)):
+        h_si = 0.0
+        for j in range(len(prob_matrix_cond[i])):
+            if prob_matrix_cond[i][j] != 0.0:
+                h_si += -1 * prob_matrix_cond[i][j] * log2(prob_matrix_cond[i][j])
+        h = h_si * sum(prob_matrix[i])
+        entropy += h
+    return entropy
+
+def task():
+    matrix = get_vals()
+    prob_matrix = prmatrix(matrix)
+    entropy_A = sentropy(prob_matrix, by='row')
+    entropy_B = sentropy(prob_matrix, by='column')
+    entropy_AB = jentropy(prob_matrix)
+    entropy_B_A = centropy(matrix, prob_matrix)
+    information_A_B = entropy_B - entropy_B_A
+    return entropy_A, entropy_B, entropy_AB, entropy_B_A, information_A_B
+
+
+task()
